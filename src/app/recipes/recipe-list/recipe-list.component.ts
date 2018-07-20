f730f6733ecae20bb365ca994d05cacf44db7669
Chandu Balla <balla.chandrasekhar@outlook.com>
2018-07-20 20:01:31 +0200

    Added shopping list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Recipe } from '../recipes.model';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,8 +7,8 @@ import { Recipe } from '../recipes.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-recipes: Recipe [] = [
-  new Recipe('Test Recipe', 'This is a test Recipe', 'https://www.google.de/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwi8lqfVuKncAhWMCOwKHQrkAFUQjRx6BAgBEAU&url=http%3A%2F%2Fwww.kraftrecipes.com%2Frecipes%2Fquick-jambalaya-recipe-56799.aspx&psig=AOvVaw3O-nHetU-wlMNWD-q9AepF&ust=1532030541432538')
+recipes: Recipe[] = [
+  new Recipe('Test Recipe', 'This is a test Recipe', 'http://www.m5zn.com/newuploads/2016/11/30/jpg//m5zn_52f1919b5820553.jpg')
 ];
 
   constructor() { }
